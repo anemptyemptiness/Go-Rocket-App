8046c33294b2b71d19badf753e4c09dd71d8a6cc
anemptyemptiness <dnerusskov1998@bk.ru>
2026-04-24 03:59:03 +0300

    fix converter

--- inventory/internal/converter/inventory.go
+++ inventory/internal/converter/inventory.go
@@ -47,15 +47,7 @@ func PartsModelToProto(parts []model.Part) []*inventoryv1.Part {
 	protoParts := make([]*inventoryv1.Part, 0, len(parts))
 
 	for _, part := range parts {
-		protoParts = append(protoParts, &inventoryv1.Part{
-			Uuid:          part.UUID,
-			Name:          part.Name,
-			Description:   part.Description,
-			Price:         part.Price,
-			PartType:      inventoryv1.PartType(part.PartType),
-			StockQuantity: part.StockQuantity,
-			CreatedAt:     timestamppb.New(part.CreatedAt),
-		})
+		protoParts = append(protoParts, PartModelToProto(part))
 	}
 
 	return protoParts
